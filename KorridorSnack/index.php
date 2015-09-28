06f07032fe0f81d704a5e0db4b302d3ff6b5da22
Marialundqvist <marialundqvists@gmail.com>
2015-09-28 15:24:13 +0200

    index mall
    ändrat css och js länkar

--- KorridorSnack/index.php
+++ KorridorSnack/index.php
@@ -7,10 +7,10 @@
       <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.8.3/jquery.min.js"></script>
       <script src="http://code.jquery.com/ui/1.9.2/jquery-ui.js"></script>
       <script type="text/javascript" src="js/hej.js"></script>
-      <script type="text/javascript" src="js/new.js"></script>
+      <script type="text/javascript" src="js/bootstrap.min.js"></script>
       <link href='http://fonts.googleapis.com/css?family=Oswald' rel='stylesheet' type='text/css'>
-      <link rel="stylesheet" type="text/css" href="css/main.css">
-      <link rel="stylesheet" type="text/css" href="css/mobile.css">
+      <link rel="stylesheet" type="text/css" href="CSS/main.css">
+      <link rel="stylesheet" type="text/css" href="CSS/special.css">
       <meta charset="utf-8">
 </head>
 
