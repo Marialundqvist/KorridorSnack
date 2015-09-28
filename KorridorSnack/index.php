9e6364cc3601f84998005f122e4dec6de3da3153
mathildaprim <matpri14@student.hh.se>
2015-09-28 15:18:03 +0200

    uppdaterat igen
    hejhej

---
+++ KorridorSnack/index.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html>
+<head>
+<meta name="viewport" content="width=device-width">
+      <link rel="shortcut icon" href="favicon.ico" />
+      <link href='http://fonts.googleapis.com/css?family=Bangers' rel='stylesheet' type='text/css'>
+      <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.8.3/jquery.min.js"></script>
+      <script src="http://code.jquery.com/ui/1.9.2/jquery-ui.js"></script>
+      <script type="text/javascript" src="js/hej.js"></script>
+      <script type="text/javascript" src="js/new.js"></script>
+      <link href='http://fonts.googleapis.com/css?family=Oswald' rel='stylesheet' type='text/css'>
+      <link rel="stylesheet" type="text/css" href="css/main.css">
+      <link rel="stylesheet" type="text/css" href="css/mobile.css">
+      <meta charset="utf-8">
+</head>
+
+
+
+<body>
+
+</body>
+</html>
