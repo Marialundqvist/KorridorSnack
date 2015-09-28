45cc4b04b31697e353d14bf5285f9c575bbf31cb
Jonas Nilsson [jonnil13] <jonnil13@ided311-10466.hh.se>
2015-09-28 15:25:52 +0200

    fixade i index

--- KorridorSnack/index.php
+++ KorridorSnack/index.php
@@ -6,7 +6,6 @@
       <link href='http://fonts.googleapis.com/css?family=Bangers' rel='stylesheet' type='text/css'>
       <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.8.3/jquery.min.js"></script>
       <script src="http://code.jquery.com/ui/1.9.2/jquery-ui.js"></script>
-      <script type="text/javascript" src="js/hej.js"></script>
       <script type="text/javascript" src="js/bootstrap.min.js"></script>
       <link href='http://fonts.googleapis.com/css?family=Oswald' rel='stylesheet' type='text/css'>
       <link rel="stylesheet" type="text/css" href="CSS/main.css">
