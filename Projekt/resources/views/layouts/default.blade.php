b719abb40afad4fd5a4aa43d1170dd2e9ba6e253
Jonnil13 <jonnil13@student.hh.se>
2015-10-07 14:05:55 +0200

    fixat template

---
+++ Projekt/resources/views/layouts/default.blade.php
@@ -0,0 +1,18 @@
+<!DOCTYPE HTML>
+<html>
+
+<link href="{{URL::asset('public/bootstrap/css/bootstrap.min.css')}}" type="text/css" rel="stylesheet">
+
+<head>
+
+</head>
+<body>
+
+	<div class="container">
+
+		@yield("innehall")
+
+	</div>	
+
+</body>
+</html>
