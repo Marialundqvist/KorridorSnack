5fe8d7c308aa8b14ae02f697b1e815ba284823d9
mathildaprim <matpri14@student.hh.se>
2015-10-08 11:17:50 +0200

    NY forstasida + css

--- Projekt/resources/views/forstasida.blade.php
+++ Projekt/resources/views/forstasida.blade.php
@@ -48,7 +48,7 @@
                   </ul>
 
 
-    <div id="inlagg">
+    
       <div class="dropdown">
 
       <label for="sel1">Forum</label>
@@ -59,36 +59,24 @@
         <option>Event</option>
         <option>Övrigt</option>
       </select>
+</div>
 
-  </div>
-          <input type="text" class="form-control" placeholder="Inlägg">
-      </div>
-
-
-       <div id="knapp">
-        <button type="submit" class="btn btn-default">Skicka</button>        
-      </div>
-    
-<div class="container-fluid">
+        <textarea class="form-control" rows="5" id="comment" placeholder="Inlägg"></textarea>
   
-      <div id="inlagg2">
-          <input type="text" class="form-control" placeholder="Inlägg">
-      </div>
 
-      <div id="knapp2">
         <button type="submit" class="btn btn-default">Skicka</button>        
-      </div>
 
+    
+<div class="container-fluid" id="morklilaruta">
+  <p>här ska inlägg kallas in</p>
+   
 </div><!-- /.container-fluid -->
 
-
-
-
-                                    <!--
-                  <div class="senaste">
-                    <a href="#">Senaste</a><p>/</p><a href="#">Populärast</a>
-                  </div>-->
-
+<div class="container-fluid" id="ljuslilaruta">
+   <input type="text" class="form-control" placeholder="Svara">
+   <button type="submit" class="btn btn-default">Skicka</button> 
+   
+</div><!-- /.container-fluid -->
 
                </div>
                <div class="forum">
