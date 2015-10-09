2db5490836cacdbdee6dac629fce9f0446d043b0
mathildaprim <matpri14@student.hh.se>
2015-10-09 14:53:22 +0200

    inlagg.blade.php
    Lägg i template-mappen

---
+++ Projekt/resources/views/includes/inlagg.blade.php
@@ -0,0 +1,19 @@
+    <div id="inlaggsruta">
+        <textarea class="form-control" rows="5" id="comment" placeholder="Inlägg"></textarea>
+        </div>
+
+        <div id="forstaknapp">
+        <button type="submit" class="btn btn-default">Skicka</button>
+        </div>        
+
+    
+<div class="container-fluid" id="morklilaruta">
+  <p>här ska inlägg kallas in</p>
+   
+</div><!-- mörklila ruta stängs -->
+
+<div class="container-fluid" id="ljuslilaruta">
+   <input type="text" class="form-control" placeholder="Svara">
+   <button type="submit" class="btn btn-default">Skicka</button> 
+   
+</div><!-- ljuslila ruta stängs -->
